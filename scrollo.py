56e5508832b4df9caba11469c77083989e953757
晓kkkk <31680619+liu1477191139@users.noreply.github.com>
2020-10-28 13:21:05 +0800

    删除无关文件

--- scrollo.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(r'C:\PycharmProjects\TB\chromedriver.exe')
-driver.get('https://www.jd.com/')
-driver.execute_script('window.scrollTo(window.pageXOffset, document.body.scrollHeight)')
--- time.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-
-t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-y = '2020-10-25 00:00:00.000002'
-z = '2020-10-25 00:00:00'
-
-print(y < z)
